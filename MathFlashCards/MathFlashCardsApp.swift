05e7700b7bdb81149c50f0c5a89767617ce57c31
Dan Beers <spanyell@icloud.com>
2025-07-05 14:43:27 -0700

    Initial Commit

---
+++ MathFlashCards/MathFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathFlashCardsApp.swift
+//  MathFlashCards
+//
+//  Created by Dan Beers on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MathFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
